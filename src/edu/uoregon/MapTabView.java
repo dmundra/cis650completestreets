1d59b870767a35910065a9edabdc1139caf23281
Daniel Mundra <dmundra@users.noreply.github.com>
2010-01-25 03:32:53 +0000

    Added the google map view to display the map.

--- src/edu/uoregon/MapTabView.java
+++ src/edu/uoregon/MapTabView.java
@@ -3,14 +3,19 @@ package edu.uoregon;
 import android.os.Bundle;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
 public class MapTabView extends MapActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.maptabview);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maptabview);
+
+		MapView mapView = (MapView) findViewById(R.id.mapView);
+		mapView.setBuiltInZoomControls(true);
+		mapView.setSatellite(true);
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
