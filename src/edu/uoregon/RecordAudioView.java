a2603c09852f3024913af8b6bc4250414aa4e900
awittig82 <awittig82@gmail.com>
2010-02-02 00:35:28 +0000

    plays audio. Need to save/restore audio from db

--- src/edu/uoregon/RecordAudioView.java
+++ src/edu/uoregon/RecordAudioView.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.IOUtils;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -25,6 +26,8 @@ public class RecordAudioView extends Activity{
 	private static final String TAG = "RecordTabView";
 	//this is what we'll use to do our current recording:
 	private final MediaRecorder recorder = new MediaRecorder();
+	//this is what we'll use to play audio:
+	private final MediaPlayer mp = new MediaPlayer();
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -35,6 +38,8 @@ public class RecordAudioView extends Activity{
 		//our geo info
 		final GeoStamp geoStamp = new GeoStamp(edu.uoregon.MapTabView.currentLocation);
 		
+		//for now we'll just use one file at a time:
+		final String fileName = "" + geoStamp.getDatabaseID();
 
 		//our buttons:
 		final Button backB = (Button) findViewById(R.id.backB);
@@ -43,12 +48,18 @@ public class RecordAudioView extends Activity{
 		//these are for switching the text on the record button:
 		final String stopRecordingAudio = getString(R.string.audioStopRecording);
 		final String startRecordingAudio = getString(R.string.audioStartRecording);
-		
+		final Button stopStartB = (Button) findViewById(R.id.playStopB);
 		
 		backB.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				//just return to our caller
+				//first let's save our current audio file (if we have one):
+				if(new File(getAudioFilePath(fileName)[1]).exists()){
+					//something is there:
+		        	//TODO: save to db
+				}
+				
+				//now return to our caller
 				finish();
 			}
 		});
@@ -60,7 +71,7 @@ public class RecordAudioView extends Activity{
 				//start recording if our text is not stop taking audio:
 				if(!recordButton.getText().equals(stopRecordingAudio)){
 					//for now, we'll only have one audio per stamp
-					if(!setUpAudio("" + geoStamp.getDatabaseID())){
+					if(!setUpAudio(fileName)){
 						Toast.makeText(RecordAudioView.this, "error with the audio, check with support", Toast.LENGTH_LONG);
 					}else{
 						recorder.start();
@@ -71,16 +82,81 @@ public class RecordAudioView extends Activity{
 					recorder.stop();
 	                recorder.reset();
 	                
+	                //do we want to save right now?
+	                
 	                recordButton.setText(startRecordingAudio);
-					//TODO: show 'play' button if we successfully recorded something
+
+	                //now we want to be able to play the audio:   
+	                showAudio(fileName, stopStartB);
+	                
 				}
 			}
 		});
 		
 		
+		//play/stop button:
+		final String stopPlay = getString(R.string.audioStopPlay);
+		final String startPlay = getString(R.string.audioStartPlay);
+		
+		//default text:
+		stopStartB.setText(startPlay);
+		
+		//TODO: get audio out of db and put it in file form
+		
+		//now we want to be able to play the audio:   
+        showAudio(fileName, stopStartB);
+		
+		stopStartB.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				//see if we are in play mode:
+				if(stopStartB.getText().equals(startPlay)){
+					//TODO: then play it:
+					try{
+						mp.reset();
+	                	mp.setDataSource(getAudioFilePath(fileName)[1]);
+	                	mp.prepare();
+	                	mp.start();
+	                	
+	                	stopStartB.setText(stopPlay);
+	                	
+					}catch(Exception e){
+						Log.e(TAG, "couldn't play the sound: " + e.toString());
+					}
+	                
+				}else{
+					
+					try{
+						mp.stop();
+					}catch(IllegalStateException e){
+						//something went wrong with playing it?
+						Log.e(TAG, "on stop: " + e.toString());
+					}
+					
+					stopStartB.setText(startPlay);
+				}
+			}
+		});
 	}
 
 
+	
+	
+	private static byte[] getAudioFileByes(String fileName) throws FileNotFoundException, IOException{
+		return IOUtils.toByteArray(new FileReader(getAudioFilePath(fileName)[1]), "UTF8");
+	}
+    
+	/**
+	 * this is a helper for getting back the file object for the audio recording we have
+	 */
+	private static String[] getAudioFilePath(String fileName){
+		final String[] ret = new String[2];
+		
+		ret[0] = Environment.getExternalStorageDirectory() + "/CompleteStreets/";
+		ret[1] = ret[0] + fileName + ".3gp";
+		
+		return ret;
+	}
+	
 	/**
      * sets up our recorder, not sure if this is a good way to do it...
      * @return false if we know something went wrong
@@ -126,21 +202,15 @@ public class RecordAudioView extends Activity{
         return true;
     }
 	
-	private static byte[] getAudioFileByes(String fileName) throws FileNotFoundException, IOException{
-		return IOUtils.toByteArray(new FileReader(getAudioFilePath(fileName)[1]), "UTF8");
-	}
-    
 	/**
-	 * this is a helper for getting back the file object for the audio recording we have
+	 * just a helper for showing the audio button
 	 */
-	private static String[] getAudioFilePath(String fileName){
-		final String[] ret = new String[2];
-		
-		ret[0] = Environment.getExternalStorageDirectory() + "/CompleteStreets/";
-		ret[1] = ret[0] + fileName + ".3gp";
-		
-		return ret;
-	}
+	private void showAudio(String fileName, Button toShow) {
+		if(new File(getAudioFilePath(fileName)[1]).exists()){
+			//something is there, let's say we can play:
+        	toShow.setVisibility(View.VISIBLE);
+		}
+    }
 
 }
 
