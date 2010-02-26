9a2d3a4d28209b1130d8c76ac54dc9c250afcfb4
awittig82 <awittig82@gmail.com>
2010-02-26 07:47:50 +0000

    border fixes

--- src/edu/uoregon/Border.java
+++ src/edu/uoregon/Border.java
@@ -1,21 +1,47 @@
 package edu.uoregon;
 
+import java.util.ArrayList;
+
 public enum Border {
 
-	NONE(0.0, 0.0, 0.0, 0.0),
-	EUGENE(32.3214, -123.322, 32.123, -123.322),
-	SANFRAN(37.7889544, -122.418728, 37.786699, -122.41309583);
+	NONE(0.0, 0.0, 0.0, 0.0, "None"),
+	EUGENE(32.3214, -123.322, 32.123, -123.322, "Eugene"),
+	SAN_FRAN(37.7889544, -122.418728, 37.786699, -122.41309583, "San Fran");
 
 	public final double top;
 	public final double left;
 	public final double right;
 	public final double bottom;
+	public final String niceName;
 
-	Border(double top, double left, double bottom, double right) {
+	Border(double top, double left, double bottom, double right, String niceName) {
 		this.top = top;
 		this.left = left;
 		this.right = right;
 		this.bottom = bottom;
+		this.niceName = niceName;
+	}
+	
+	public static String[] getNiceNames(){
+		final String[] names = new String[Border.values().length];
+		//final ArrayList<String> names = new ArrayList<String>();
+//		for(Border b : Border.values()){
+//			names.add(b.niceName);
+//		}
+		for(int i = 0; i < Border.values().length; ++i){
+			names[i] = Border.values()[i].niceName;
+		}
+		return names;
+	}
+	
+	public static Border getByNiceName(String niceName){
+		for(Border b : Border.values()){
+			if(niceName.equals(b.niceName)){
+				return b;
+			}
+		}
+		
+		return NONE;
 	}
 
 }
